ad4ad368cb6ac9525677eaf0fe4ad482bef28123
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-12-16 18:47:05 +0100

    task 1

--- pall.c
+++ pall.c
@@ -26,7 +26,7 @@ void pint(stack_t **top, int line_number)
 {
 	if (*top == NULL)
 	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	else
