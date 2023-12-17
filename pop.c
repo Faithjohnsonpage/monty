721c0ee4e73e6154a8e2afbd3306f8b52c7aff81
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-12-17 04:02:25 +0100

    task 2

--- pop.c
+++ pop.c
@@ -10,7 +10,7 @@ void pop(stack_t **top, int line_number)
 {
 	stack_t *temp = *top;
 
-	if (top == NULL)
+	if (*top == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
