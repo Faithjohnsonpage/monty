b3e54840be642070a72f107ffde45190ca0d556e
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-12-18 01:55:12 +0100

    task 13

--- rotl.c
+++ rotl.c
@@ -1,8 +1,18 @@
 #include "monty.h"
 
+/**
+ * rotl - The opcode rotl rotates the stack to the top.
+ * @top: A pointer to the top of stack.
+ */
+
 void rotl(stack_t **top)
 {
 	stack_t *first = NULL, *second = NULL, *last = NULL;
+
+	if (*top == NULL || (*top)->next == NULL)
+	{
+		return;
+	}
 
 	first = *top;
 	second = (*top)->next;
